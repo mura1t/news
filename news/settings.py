e1bd7fba7caea5c8fbcf147bb7c329b7275abc87
Murat Karabacak <m9urat@gmail.com>
2018-09-26 19:07:22 +0300

    optimization

--- news/settings.py
+++ news/settings.py
@@ -149,7 +149,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-
+SITE_ID = 1
 STATIC_URL = '/staticfiles/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "staticfiles"),
